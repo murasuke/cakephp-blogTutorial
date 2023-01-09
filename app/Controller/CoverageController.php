b91897da3c34dbc4b020a4b0207fbe36e2fa7b95
tkyk.niimura <tkyk.niimura@gmai.com>
2023-01-09 21:42:25 +0900

    fix code

--- app/Controller/CoverageController.php
+++ app/Controller/CoverageController.php
@@ -1,4 +1,7 @@
 <?php
+use SebastianBergmann\CodeCoverage\CodeCoverage;
+use SebastianBergmann\CodeCoverage\Report\Html\Facade;
+
 App::uses('AppController', 'Controller');
 /**
  *  Coverage Controller
@@ -13,27 +16,20 @@ class  CoverageController extends AppController {
 	public function index() {
         error_reporting(0);
         $this->autoRender = false;
-        $this->autoLayout = false;
 
         $coverages = glob(TMP.'/coverage/coverage-*.json');
 
-        $finalCoverage = new SebastianBergmann\CodeCoverage\CodeCoverage();
-        $finalCoverage->filter()->addDirectoryToWhitelist(ROOT.DS.APP_DIR);
+        $codeCoverage = new CodeCoverage();
+        $codeCoverage->filter()->addDirectoryToWhitelist(ROOT.DS.APP_DIR);
 
-
-        $count = count($coverages);
         foreach ($coverages as $index => $coverageFile)
         {
-            // echo 'Processing coverage (' . (string)($index + 1) . "/$count) from $coverageFile". PHP_EOL;
             $codecoverageData = json_decode(file_get_contents($coverageFile), JSON_OBJECT_AS_ARRAY);
-            $testName = str_ireplace(basename($coverageFile,".json"),"coverage-", "");
-            $finalCoverage->append($codecoverageData, $testName);
+            $codeCoverage->append($codecoverageData, $index);
         }
 
-        // echo "Generating final report..." . PHP_EOL;
-        $report = new \SebastianBergmann\CodeCoverage\Report\Html\Facade();
-        $report->process($finalCoverage, WWW_ROOT.'report');
-        // echo "Report generated succesfully". PHP_EOL;
-        $this->redirect('/app/webroot/report/', 301);
+        $report = new Facade();
+        $report->process($codeCoverage, WWW_ROOT.'report');
+        $this->redirect('/app/webroot/report/');
 	}
 }
--- app/Lib/coverage_inc.php
+++ app/Lib/coverage_inc.php
@@ -1,20 +1,18 @@
 <?php
-
-xdebug_start_code_coverage(XDEBUG_CC_UNUSED | XDEBUG_CC_DEAD_CODE);
-
+/**
+ * CoverageDumper
+ * https://xdebug.org/docs/code_coverage#xdebug_get_code_coverage
+ */
 class CoverageDumper
 {
-    private $testName;
-
-    function __construct( $testName)
+    function __construct()
     {
-        $this->testName = $testName;
+        xdebug_start_code_coverage(XDEBUG_CC_UNUSED | XDEBUG_CC_DEAD_CODE);
     }
 
     function __destruct()
     {
-        // $coverageName = '/var/www/html/blogTutorial/app/tmp/coverage/coverage-' . $this->testName . '-' . microtime(true);
-        $coverageName = TMP.'/coverage/coverage-' . $this->testName . '-' . microtime(true);
+        $coverageName = TMP.'coverage/coverage-' . microtime(true);
         try {
             xdebug_stop_code_coverage(false);
             $codecoverageData = json_encode(xdebug_get_code_coverage());
@@ -25,31 +23,5 @@ class CoverageDumper
     }
 }
 
-$testName = (isset($_COOKIE['test_name']) && !empty($_COOKIE['test_name'])) ? $_COOKIE['test_name'] : 'unknown_test_' . time();
-$_coverageDumper = new CoverageDumper($testName);
-
-// require_once dirname(__FILE__).'/../../vendors/phpunit/php-code-coverage/src/CodeCoverage.php';
-// require_once dirname(__FILE__).'/../../vendors/phpunit/php-code-coverage/src/Filter.php';
-// $filter = new \SebastianBergmann\CodeCoverage\Filter();
-// $filter->addDirectoryToWhitelist($base.'/app');
-
-
-// $coverage = new \SebastianBergmann\CodeCoverage\CodeCoverage(null, $filter);
-// $coverage->start('phpcov');
-
-// $GLOBALS['cakephp_codecoverage'] = $coverage;
-
-
-
-
-
-// function afterFilter() {
-//     $coverage = $GLOBALS['cakephp_codecoverage'];
-//     $coverage->stop();
-
-//     require_once dirname(__FILE__).'/../../vendors/phpunit/php-code-coverage/src/Report/Html/Facade.php';
-//     $writer = new \SebastianBergmann\CodeCoverage\Report\Html\Facade;
-//     $writer->process($coverage, getcwd().'/coverage');
-// }
-
+$_coverageDumper = new CoverageDumper();
 
